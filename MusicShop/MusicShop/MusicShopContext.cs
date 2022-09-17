8b8f5bf0d3d023f9525df6235b006e25abec1e16
blackscreenmk <blackscreen.mk@gmail.com>
2022-09-17 20:42:32 +0300

    Add User table

--- MusicShop/MusicShop/MusicShopContext.cs
+++ MusicShop/MusicShop/MusicShopContext.cs
@@ -12,7 +12,7 @@ namespace MusicShop
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MusicShop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
