99b8f5aa12801e93e77564a3f47cf3876f56a997
Student <student@if.itstep>
2022-09-17 19:15:15 +0300

    Add table Author

---
+++ MusicShop/MusicShop/Configuration/AuthorConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace MusicShop
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.Surname).IsRequired();
+            builder.Property(p => p.Pseudonym).IsRequired();
+            builder.Property(p => p.Birthdaydate).IsRequired();
+        }
+    }
+}
--- MusicShop/MusicShop/MusicShopContext.cs
+++ MusicShop/MusicShop/MusicShopContext.cs
@@ -4,12 +4,12 @@ namespace MusicShop
 {
     internal class MusicShopContext : DbContext
     {
-
+        public DbSet<Author> Authors { get; set; }
         public MusicShopContext() { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Shop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
