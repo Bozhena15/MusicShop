b591478e48bbe746d90b81f9987b73f6b3245bd7
DaRkInss <popovichwws232@gmail.com>
2022-09-18 21:33:04 +0300

    Add User Order

---
+++ MusicShop/MusicShop/Migrations/20220918181826_AddUserOrder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicShop.Migrations
+{
+    public partial class AddUserOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserOrder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlateCount = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserOrder", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserOrder_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOrder_UserId",
+                table: "UserOrder",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserOrder");
+        }
+    }
+}
--- MusicShop/MusicShop/Models/UserOrder.cs
+++ MusicShop/MusicShop/Models/UserOrder.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace MusicShop.Models
 {
-    public class User
+    public class UserOrder
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string UserSurname { get; set; }
-        public List<UserOrder> userOrders { get; set; }
+        public int PlateCount { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        
     }
 }
