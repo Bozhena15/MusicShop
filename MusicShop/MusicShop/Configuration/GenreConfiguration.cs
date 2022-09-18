60f8d06998c458aacdceea98a7dc815a851775c9
Chiaok <bazylivillya@gmail.com>
2022-09-18 21:16:22 +0300

    add Genre table

---
+++ MusicShop/MusicShop/Configuration/GenreConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Configuration
+{
+    internal class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.MusicStyle).IsRequired();
+        }
+    }
+}
