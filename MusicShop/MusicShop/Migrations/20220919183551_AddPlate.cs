fd300348935dab5c8c2eca7159049742b2daa9d2
Chiaok <bazylivillya@gmail.com>
2022-09-20 21:28:32 +0300

    add files

---
+++ MusicShop/MusicShop/Migrations/20220919183551_AddPlate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicShop.Migrations
+{
+    public partial class AddPlate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    EditionId = table.Column<int>(type: "int", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    TrackCount = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PlatePrice = table.Column<int>(type: "int", nullable: false),
+                    PlateCost = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Plates_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Plates_Editions_EditionId",
+                        column: x => x.EditionId,
+                        principalTable: "Editions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Plates_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plates_AuthorId",
+                table: "Plates",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plates_EditionId",
+                table: "Plates",
+                column: "EditionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plates_GenreId",
+                table: "Plates",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plates");
+        }
+    }
+}
---
+++ MusicShop/MusicShop/Models/Plate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Models
+{
+    public class Plate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GenreId { get; set; }
+        public int EditionId { get; set; }
+        public int AuthorId { get; set; }
+        public int TrackCount { get; set; }
+        public DateTime Year { get; set; }
+        public int PlatePrice { get; set; }
+        public int PlateCost { get; set; }
+        public Genre Genre { get; set; }
+        public Edition Edition { get; set; }
+        public Author Author { get; set; }
+    }
+}
