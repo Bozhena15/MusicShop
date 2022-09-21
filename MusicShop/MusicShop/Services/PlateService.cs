f4a9c2e052b261b21f6bc84cc1782f7b965c1307
blackscreenmk <blackscreen.mk@gmail.com>
2022-09-21 22:15:15 +0300

    Add Plate service

--- MusicShop/MusicShop/Services/PlateService.cs
+++ MusicShop/MusicShop/Services/PlateService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace MusicShop.Services
 {
-    internal class PlateService
+    public class PlateService
     {
+        private readonly MusicShopContext musicshopcontext;
+
+        public PlateService(MusicShopContext musicshopcontext)
+        {
+            this.musicshopcontext = musicshopcontext;
+        }
+
+        public void AddPlate(Plate newPlate)
+        {
+            if(newPlate == null)
+                throw new Exception($"{nameof(Plate)} cannot be null");
+
+            musicshopcontext.Plates.Add(newPlate);
+            musicshopcontext.SaveChangesAsync();
+        }
+
+        public void Delete(int id)
+        {
+            Plate? plate = musicshopcontext.Plates.FirstOrDefault(x=> x.Id == id);
+
+            if (plate == null)
+                throw new Exception("Wrong object id to delete");
+
+            musicshopcontext.Plates.Remove(plate);
+            musicshopcontext.SaveChanges();
+        }
+
+        public IEnumerable<Plate> GetPlates()
+        {
+            var plates = musicshopcontext.Plates.ToList();
+
+            return plates;
+        }
     }
 }
