d7feb4248494668c7066fe1b750522c73b00b624
Chiaok <bazylivillya@gmail.com>
2022-09-23 20:54:55 +0300

    Add Edition Service

---
+++ MusicShop/MusicShop/Services/EditionService.cs
@@ -0,0 +1,44 @@
+﻿using MusicShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Services
+{
+    public class EditionService
+    {
+        private readonly MusicShopContext _musicShopContext;
+
+        public EditionService(MusicShopContext musicShopContext)
+        {
+            _musicShopContext = musicShopContext;
+        }
+        public IEnumerable<Edition> GetEditions()
+        {
+            var editions = _musicShopContext.Users.ToList();
+            return (IEnumerable<Edition>)editions;
+        }
+        public void AddNewEdition(Edition edition)
+        {
+            if (edition == null)
+                throw new NullReferenceException(nameof(edition));
+            _musicShopContext.Editions.Add(edition);
+            _musicShopContext.SaveChanges();
+            
+        }
+        public void DelteUser(int id)
+        {
+            var edition = _musicShopContext.Editions.FirstOrDefault(x => x.Id == id);
+            if (edition == null)
+                throw new NullReferenceException(nameof(edition));
+            _musicShopContext.Editions.Remove(edition);
+            _musicShopContext.SaveChanges();
+
+
+            
+        }
+
+    }
+}
