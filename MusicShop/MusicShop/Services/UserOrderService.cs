5bd5b22b3f925adf016cf72935725d3c28ddc2b0
Student <student@if.itstep>
2022-09-24 19:46:37 +0300

    Add User Order Service

--- MusicShop/MusicShop/Services/UserOrderService.cs
+++ MusicShop/MusicShop/Services/UserOrderService.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using MusicShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace MusicShop.Services
 {
-    internal class Class1
+    public class UserOrderService
     {
+        private readonly MusicShopContext _musicShopContext;
+
+        public UserOrderService(MusicShopContext musicShopContext)
+        {
+            _musicShopContext = musicShopContext;
+        }
+
+        public IEnumerable<UserOrder> GetUsers()
+        {
+            var orders = _musicShopContext.UserOrders.ToList();
+            return orders;
+        }
+
+        public void AddNewUser(UserOrder order)
+        {
+            if (order == null)
+                throw new NullReferenceException(nameof(order));
+
+            _musicShopContext.UserOrders.Add(order);
+            _musicShopContext.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            var order = _musicShopContext.UserOrders.FirstOrDefault(x => x.Id == id);
+            if (order == null)
+                throw new NullReferenceException(nameof(order));
+
+            _musicShopContext.UserOrders.Remove(order);
+            _musicShopContext.SaveChanges();
+        }
     }
 }
